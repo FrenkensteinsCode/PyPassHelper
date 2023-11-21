0963c1596b171f1ea177d4d0d8605ecf978ce9fe
FrenkensteinsCode <>
2023-11-21 22:31:52 +0100

    Add first tests

---
+++ tests/Backend_Test.py
@@ -0,0 +1,55 @@
+import os
+import platform
+import re
+import sys
+import unittest
+
+sys.path.append("../PyPassHelper")
+from PyPassHelper_backend import *
+
+class Test_Backend_Methods(unittest.TestCase):
+
+    def setUp(self):
+        if platform.system() == "Windows":
+            self.separator = "\\"
+        else:
+            self.separator = "/"
+        self.key_location = os.getcwd() + self.separator + "secret.key"
+        self.pwfile_location = os.getcwd() + self.separator + "pwfile.txt"
+        self.key_location_exists = False
+        self.pwfile_location_exists = False
+        self.pw_contains_all_chars = False
+
+    def tearDown(self):
+        if self.key_location_exists:
+            os.remove(self.key_location)
+        if self.pwfile_location_exists:
+            os.remove(self.pwfile_location)
+
+    def test_key_is_created_correctly(self):
+        key_gen(self.key_location)
+        self.assertTrue(os.path.isfile(self.key_location))
+        self.key_location_exists = True
+
+    def test_pwfile_is_created_correctly(self):
+        write_password("Test_Pass","Test_Service", self.pwfile_location)
+        self.assertTrue(os.path.isfile(self.pwfile_location))
+        self.pwfile_location_exists = True
+
+    def test_create_password(self):
+        password = create_password(8)
+        if re.search(r'\d', password) and re.search(r'[A-Z]', password) and re.search(r'[a-z]', password) and re.search(r'\W', password):
+            self.pw_contains_all_chars = True
+
+        self.assertTrue(self.pw_contains_all_chars)
+
+    def test_shuffle_is_working_as_expected(self):
+        pre_shuffle = create_password(8)
+        post_shuffle = shuffle(pre_shuffle, 8)
+        self.assertFalse(pre_shuffle == post_shuffle)
+
+    def test_create_passphrase(self):
+        pass # Idea is to create a passphrase from predefined wordlists and to check it against the expected string...will implement this later
+
+if __name__ == "__main__":
+    unittest.main()
