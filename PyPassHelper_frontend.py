c1afc0bfb0507832e9cd33bca9781ee42ca04f88
FrenkensteinsCode <>
2023-11-28 23:19:29 +0100

    Update version number

--- PyPassHelper_frontend.py
+++ PyPassHelper_frontend.py
@@ -12,7 +12,7 @@ def get_info_dialog():
 **********************\n\
 Title: PyPassHelper\n\
 Author: @FrenkensteinsCode\n\
-Version: 1.0.0\n\
+Version: 1.1.0\n\
 **********************"
     messagebox.showinfo(message=m_text, title = "About")
 
